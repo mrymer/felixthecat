fd14ce23456504d0fc63127a15545d0184afd83f
Maria Rymer <mariarymer@marias-mbp.usny.ibm.com>
2017-08-30 17:06:37 -0400

    add create and get all routes

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const uuidv1 = require('uuid/v1');
 const app = express()
+let database = {}
+
+app.get('/create', function(req, res){
+    const newAnimal = {
+      name: 'tom',
+      type: 'cat'
+    }
+    const id = uuidv1()
+    database[id] = newAnimal
+    res.json(id)
+})
+
+app.get('/get-all', function(req,res){
+  res.json(database)
+})
 
 app.get('/', function(req, res){
   res.send('Como estas?')
